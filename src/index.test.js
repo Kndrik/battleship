ce253ac84a5b7a11c8542d20113c143164c2b085
Charles <charles.lamarque69@gmail.com>
2023-03-08 11:16:06 +0100

    Create ship factory and tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,34 @@
 import { sum } from "./index";
+import Ship from "./factories/Ship";
 
-test('test', () => {
-    expect(sum(1,2)).toBe(3);
+test('Ship not hit', () => {
+    const ship = new Ship(3);
+    expect(ship.hitCount).toBe(0);
+});
+
+test('Ship hit', () => {
+    const ship = new Ship(4);
+    ship.hit();
+    ship.hit();
+    expect(ship.hitCount).toBe(2);
+});
+
+test('Ship: isSunk() -> no hit', () => {
+    const ship = new Ship(3);
+    expect(ship.isSunk()).toBeFalsy();
+});
+
+test('Ship: isSunk() -> some hits', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeFalsy();
+});
+
+test('Ship: isSunk() -> fully hit', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBeTruthy();
 });
